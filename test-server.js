c5016bbde0b9be674821df7af6739e41cf8c750f
Richeve Bebedor <richeve.bebedor@gmail.com>
2013-10-19 15:38:20 +0800

    Updates

---
+++ test-server.js
@@ -0,0 +1,48 @@
+var http = require( "http" );
+var url = require( "url" );
+var fs = require( "fs" );
+
+http.createServer( function( request, response ){
+	var urlObject = url.parse( request.url );
+	console.log( JSON.stringify( urlObject ) );
+	if( urlObject.pathname == "/" ){
+		fs.readFile( "./adaptable-test.html",
+			{ "encoding": "utf8" },
+			function( error, data ){
+				response.writeHead( 200, {
+					"Content-Type": "text/html"
+				} );
+				response.end( data );
+			} );
+	}else{
+		fs.exists( "." + urlObject.pathname,
+			function( exists ){
+				if( exists ){
+					fs.readFile( "." + urlObject.pathname,
+						{ "encoding": "utf8" },
+						function( error, data ){
+							var mimetype;
+							switch( urlObject.pathname.match( /(?:\.)(\w+)$/ )[ 1 ] ){
+								case "js":
+									mimetype = "text/javascript";
+									break;
+
+								case "html":
+									mimetype = "text/html";
+									break;
+
+								case "less":
+									mimetype = "text/css";
+							}
+							response.writeHead( 200, {
+								"Content-Type": mimetype
+							} );
+							response.end( data );
+						} );			
+				}else{
+					response.writeHead( 404 );
+					response.end( );
+				}
+			} );
+	}
+} ).listen( 8080, "127.0.0.1" );
